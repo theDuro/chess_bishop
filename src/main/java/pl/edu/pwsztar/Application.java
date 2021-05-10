b0f1047177165b9c5447286925f58fa753cb2adb
theDuro <52936119+theDuro@users.noreply.github.com>
2021-05-10 10:07:44 +0200

    adding contlorel

--- src/main/java/pl/edu/pwsztar/Application.java
+++ src/main/java/pl/edu/pwsztar/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 
-@SpringBootApplication
-//@EnableAutoConfiguration
+//@SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/pl/edu/pwsztar/controller/ChessApiController.java
+++ src/main/java/pl/edu/pwsztar/controller/ChessApiController.java
@@ -2,29 +2,31 @@ package pl.edu.pwsztar.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.edu.pwsztar.domain.CheasFasade;
 import pl.edu.pwsztar.domain.dto.FigureMoveDto;
 import pl.edu.pwsztar.planist.ChesMovieChecker;
+@RestController
 @RequestMapping(value="/api")
-public class ChessApiController {
-    private CheasFasade cheasFasade;
-    public ChessApiController(CheasFasade cheasFasade){
-  this.cheasFasade=cheasFasade;
-    }
 
+public class ChessApiController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ChessApiController.class);
+
+    private CheasFasade cheasFasade;
+    @Autowired
+    ChessApiController(CheasFasade cheasFasade){
+        this.cheasFasade=cheasFasade;
+    }
 
     @CrossOrigin
     @PostMapping(value = "/chess/is-correct-move")
     public ResponseEntity<Boolean> isCorrectMove(@RequestBody FigureMoveDto figureMoveDto) {
-        LOGGER.info("*** move details : {}", figureMoveDto);
+      LOGGER.info("działam");
+
         return ResponseEntity.ok(cheasFasade.itsCorectMove(figureMoveDto));
     }
 }
